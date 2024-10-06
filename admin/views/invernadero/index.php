7f7e7aebb1ea07f7dab3b082fe89c20d5ba949dc
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-05 23:27:41 -0600

    added files and added header
    added the css files for both the login and main documents

--- admin/views/invernadero/index.php
+++ admin/views/invernadero/index.php
@@ -1,32 +1,25 @@
 <?php require('views/header.php')?>
-  <h1>Invernaderos</h1>
+  <h1>Administradores</h1>
   <?php if (isset($mensaje)): $app -> alerta($tipo, $mensaje); endif;?>
-  <a href="invernadero.php?accion=crear" class="btn btn-success">Nuevo</a>
+  <a href="administrador.php?accion=crear" class="btn btn-success">Nuevo</a>
   <table class="table">
   <thead>
     <tr>
-      <th scope="col">Id invernadero</th>
+      <th scope="col">Id</th>
       <th scope="col">Nombre</th>
-      <th scope="col">Area</th>
-      <th scope="col">Latitud</th>
-      <th scope="col">Longitud</th>
-      <th scope="col">Fecha de creacion</th>
-      <th scope="col">Opciones</th>
+      <th scope="col">Contraseña</th>
     </tr>
   </thead>
   <tbody>
-    <?php foreach($invernaderos as $invernadero): ?>
+    <?php foreach($administradores as $administrador): ?>
     <tr>
-      <th scope="row"><?php echo $invernadero ['id_invernadero']; ?></th>
-      <td><?php echo $invernadero ['invernadero']; ?></td>
-      <td><?php echo $invernadero ['area']; ?></td>
-      <td><?php echo $invernadero ['latitud']; ?></td>
-      <td><?php echo $invernadero ['longitud']; ?></td>
-      <td><?php echo $invernadero ['fecha_creacion']; ?></td>
+      <td><?php echo $administrador ['id']; ?></td>
+      <td><?php echo $administrador ['nombre']; ?></td>
+      <td><?php echo $administrador ['contrasena']; ?></td>
       <td>
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <a href="invernadero.php?accion=actualizar&id=<?php echo $invernadero ['id_invernadero']; ?>" class="btn btn-warning">Actualizar</a>
-          <a href="invernadero.php?accion=eliminar&id=<?php echo $invernadero ['id_invernadero']; ?>" class="btn btn-danger">Eliminar</a>
+          <a href="administrador.php?accion=actualizar&id=<?php echo $administrador ['id']; ?>" class="btn btn-warning">Actualizar</a>
+          <a href="administrador.php?accion=eliminar&id=<?php echo $administrador ['id']; ?>" class="btn btn-danger">Eliminar</a>
         </div>
       </td>
     </tr>
