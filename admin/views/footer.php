d4457551dbbd735bc2480eee821ffd40e79c853d
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-05 23:29:18 -0600

    added footer

--- admin/views/footer.php
+++ admin/views/footer.php
@@ -1,3 +1,43 @@
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+<footer>
+    <div class="row">
+      <div class="col-md-1"></div>
+      <div class="col-md-10">
+        <div class="row">
+          <div class="col-sm-6 mb-3 mb-sm-0">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Ponte en contacto con nosotros</h5>
+                <p class="card-text">Si tienes alguna duda que aún no haya sido solucionada en tu estancia en esta
+                  página,
+                  puedes contactarnos, mediante nuestro sitio web o mediante un correo electrónico a la siguiente
+                  dirección
+                  de correo: cualquiera3013@gmail.com</p>
+                <a href="index/contact.html" class="btn btn-danger">Contacto</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Seguridad primero</h5>
+                <p class="card-text">Si nuestro sitio web no te inspira confianza, no te preocupes, puedes visitarnos
+                  cuando quieras en nuestra tienda fisica.</p>
+                <a href="index/location.html" class="btn btn-danger">Ubicacion</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-1"></div>
+    </div>
+
+    <nav class="navbar bg-body-tertiary bg-dark" data-bs-theme="dark">
+      <div class="container-fluid">
+        <span class="navbar-text">
+          Se tu mismo, expresate como eres. Nunca dejes de ser tu. Todos los derechos reservados CoolHats 2024.
+        </span>
+      </div>
+    </nav>
+  </footer>
+</body>
 </html>
