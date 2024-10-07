15120d3fef10c977df9180c70cc3e05e82b537b3
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-06 18:02:42 -0600

    usuario.php refactored

--- admin/usuario.php
+++ admin/usuario.php
@@ -1,12 +1,12 @@
 <?php
-require_once ('producto.class.php');
-$app = new producto();
+require_once ('usuario.class.php');
+$app = new usuario();
 
 $accion = (isset($_GET['accion']))?$_GET['accion'] : NULL;
 $id=(isset($_GET['id']))?$_GET['id']:null;
 switch ($accion) {
     case 'crear': {
-        include 'views/producto/crear.php';
+        include 'views/usuario/crear.php';
         break;
     }
 
@@ -14,21 +14,21 @@ switch ($accion) {
         $data=$_POST['data'];
         $resultado = $app->create($data);
         if ($resultado) {
-            $mensaje = "producto dado de alta correctamente";
+            $mensaje = "Usuario dado de alta correctamente";
             $tipo = "success";
         } else {
-            $mensaje = "El producto no ha sido dado de alta";
+            $mensaje = "El usuario no ha sido dado de alta";
             $tipo = "danger";
         }
 
-        $productos = $app->readAll();
-        include('views/producto/index.php');
+        $usuarios = $app->readAll();
+        include('views/usuario/index.php');
         break;
     }
 
     case 'actualizar': {
-        $productos = $app -> readOne($id); 
-        include('views/producto/crear.php');
+        $usuarios = $app -> readOne($id); 
+        include('views/usuario/crear.php');
         break;
     }
     
@@ -36,14 +36,14 @@ switch ($accion) {
         $data= $_POST['data'];
         $result=$app->update($id,$data);
         if($result){
-            $mensaje="El producto se ha actualizado";
+            $mensaje="El usuario se ha actualizado";
             $tipo="success";
         }else{
             $mensaje="No se ha actualizado";
             $tipo="danger";
         }
-        $productos = $app->readAll();
-        include('views/producto/index.php');
+        $usuarios = $app->readAll();
+        include('views/usuario/index.php');
         break;
     }
 
@@ -52,22 +52,22 @@ switch ($accion) {
             if (is_numeric($id)) {
                 $resultado = $app -> delete($id);
                 if ($resultado) {
-                    $mensaje = "El producto se eliminó correctamente";
+                    $mensaje = "El usuario se eliminó correctamente";
                     $tipo = "success";
                 } else {
-                    $mensaje = "El producto no se eliminó correctamente";
+                    $mensaje = "El usuario no se eliminó correctamente";
                     $tipo = "danger";
                 }
             }
         }
-        $productos = $app->readAll();
-        include('views/producto/index.php');
+        $usuarios = $app->readAll();
+        include('views/usuario/index.php');
         break;
     }
 
     default: {
-        $productos = $app->readAll();
-        include 'views/producto/index.php';
+        $usuarios = $app->readAll();
+        include 'views/usuario/index.php';
         break;
     }
 }
