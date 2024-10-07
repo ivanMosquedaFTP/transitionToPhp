cd19c173793c00b28af389c7c778cd3deb99b349
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-06 23:21:55 -0600

    admin/views/producto/crear.php refactored

--- admin/views/producto/crear.php
+++ admin/views/producto/crear.php
@@ -1,20 +1,35 @@
 <?php require('views/header.php'); ?>
 <center>
-    <h1><?php if($accion=="crear"):echo('Nuevo');else: echo ('Modificar');endif; ?> Administrador</h1>
+    <h1><?php if($accion=="crear"):echo('Nuevo');else: echo ('Modificar');endif; ?> producto</h1>
 </center>
 
 <div class="row">
     <div class="col-md-1"></div>
     <div class="col-md-10">
-        <form method="post" action="administrador.php?accion=<?php if($accion=="crear"):echo('nuevo');else:echo('modificar&id='.$id);endif;?>">
+        <form method="post" action="producto.php?accion=<?php if($accion=="crear"):echo('nuevo');else:echo('modificar&id='.$id);endif;?>">
         <div class="mb-3">
-            <label for="nombre" class="form-label">Nombre del administrador</label>
-            <input class="form-control" type="text" name="data[nombre]" placeholder="Escribe aqui el nombre" value="<?php if(isset($administradores["nombre"])):echo($administradores['nombre']);endif;?>" id="nombre"/>
+            <label for="nombre_producto" class="form-label">Nombre del producto</label>
+            <input class="form-control" type="text" name="data[nombre_producto]" placeholder="Escribe aqui el nombre del producto" value="<?php if(isset($productos["nombre_producto"])):echo($productos['nombre_producto']);endif;?>" id="nombre_producto"/>
         </div>
 
         <div class="mb-3">
-            <label for="contrasena" class="form-label">Contraseña del nuevo administrador</label>
-            <input class="form-control" type="text" name="data[contrasena]" placeholder="Escribe aqui la contraseña"  value="<?php if(isset($administradores["contrasena"])):echo($administradores['contrasena']);endif;?>"  id="contrasena"/>
+            <label for="descripcion" class="form-label">Descripción del nuevo producto</label>
+            <input class="form-control" type="text" name="data[descripcion]" placeholder="Escribe aqui la descripción del producto"  value="<?php if(isset($productos["descripcion"])):echo($productos['descripcion']);endif;?>"  id="descripcion"/>
+        </div>
+
+        <div class="mb-3">
+            <label for="precio" class="form-label">Precio del nuevo producto</label>
+            <input class="form-control" type="text" name="data[precio]" placeholder="Escribe aqui el precio del producto"  value="<?php if(isset($productos["precio"])):echo($productos['precio']);endif;?>"  id="precio"/>
+        </div>
+
+        <div class="mb-3">
+            <label for="stock" class="form-label">Stock del nuevo producto</label>
+            <input class="form-control" type="text" name="data[stock]" placeholder="Escribe aqui el stock del producto"  value="<?php if(isset($productos["stock"])):echo($productos['stock']);endif;?>"  id="stock"/>
+        </div>
+
+        <div class="mb-3">
+            <label for="fecha_publicacion" class="form-label">Fecha de publicación del nuevo producto</label>
+            <input class="form-control" type="text" name="data[fecha_publicacion]" placeholder="Escribe aqui la fecha de publicación del producto"  value="<?php if(isset($productos["fecha_publicacion"])):echo($productos['fecha_publicacion']);endif;?>"  id="fecha_publicacion"/>
         </div>
 
         <input type="submit" value="Guardar" name="data[enviar]" class="btn btn-primary w-100">
