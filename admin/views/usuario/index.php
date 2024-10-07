bc5472cf069487775c89508801c2c01703db8c33
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-06 18:04:07 -0600

    TODO

--- admin/views/usuario/index.php
+++ admin/views/usuario/index.php
@@ -1,12 +1,12 @@
 <?php require('views/header.php')?>
-  <h1>Productos</h1>
+  <h1>Usuarios</h1>
   <?php if (isset($mensaje)): $app -> alerta($tipo, $mensaje); endif;?>
-  <a href="producto.php?accion=crear" class="btn btn-success">Nuevo</a>
+  <a href="usuario.php?accion=crear" class="btn btn-success">Nuevo</a>
   <table class="table">
   <thead>
     <tr>
       <th scope="col">Id</th>
-      <th scope="col">Nombre del producto</th>
+      <th scope="col">Nombre del usuario</th>
       <th scope="col">Descripcion</th>
       <th scope="col">Precio</th>
       <th scope="col">Stock</th>
@@ -14,18 +14,18 @@
     </tr>
   </thead>
   <tbody>
-    <?php foreach($productos as $producto): ?>
+    <?php foreach($usuarios as $usuario): ?>
     <tr>
-      <td><?php echo $producto ['id']; ?></td>
-      <td><?php echo $producto ['nombre_producto']; ?></td>
-      <td><?php echo $producto ['descripcion']; ?></td>
-      <td><?php echo $producto ['precio']; ?></td>
-      <td><?php echo $producto ['stock']; ?></td>
-      <td><?php echo $producto ['fecha_publicacion']; ?></td>
+      <td><?php echo $usuario ['id']; ?></td>
+      <td><?php echo $usuario ['nombre_usuario']; ?></td>
+      <td><?php echo $usuario ['descripcion']; ?></td>
+      <td><?php echo $usuario ['precio']; ?></td>
+      <td><?php echo $usuario ['stock']; ?></td>
+      <td><?php echo $usuario ['fecha_publicacion']; ?></td>
       <td>
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <a href="producto.php?accion=actualizar&id=<?php echo $producto ['id']; ?>" class="btn btn-warning">Actualizar</a>
-          <a href="producto.php?accion=eliminar&id=<?php echo $producto ['id']; ?>" class="btn btn-danger">Eliminar</a>
+          <a href="usuario.php?accion=actualizar&id=<?php echo $usuario ['id']; ?>" class="btn btn-warning">Actualizar</a>
+          <a href="usuario.php?accion=eliminar&id=<?php echo $usuario ['id']; ?>" class="btn btn-danger">Eliminar</a>
         </div>
       </td>
     </tr>
