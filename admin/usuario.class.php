58020d647a4e89ee3f31db3bdaa32c20cdefe989
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-06 18:01:07 -0600

    usuario.class.php refactored

--- admin/usuario.class.php
+++ admin/usuario.class.php
@@ -1,18 +1,18 @@
 <?php
  require_once ('../sistema.class.php');
 
- class producto extends sistema {
+ class usuario extends sistema {
     function create ($data){
         $result = [];
         $insertar = [];
         $this -> conexion();
-        $sql="insert into producto(nombre_producto, descripcion, precio, stock) values(:nombre_producto, :descripcion, :precio, :stock);";
+        $sql="insert into usuario(nombre_completo, telefono, contrasena, email, total_compras) values(:nombre_completo, :telefono, :contrasena, :email, :total_compras);";
         $insertar = $this->con->prepare($sql);
-        $insertar -> bindParam(':nombre_producto', $data['nombre_producto'], PDO::PARAM_STR);
-        $insertar -> bindParam(':descripcion', $data['descripcion'], PDO::PARAM_STR);
-        $precio = strval($data['precio']);
-        $insertar -> bindParam(':precio', $data['precio'], PDO::PARAM_STR);
-        $insertar -> bindParam(':stock', $data['stock'], PDO::PARAM_INT);
+        $insertar -> bindParam(':nombre_completo', $data['nombre_completo'], PDO::PARAM_STR);
+        $insertar -> bindParam(':telefono', $data['telefono'], PDO::PARAM_STR);
+        $insertar -> bindParam(':contrasena', $data['contrasena'], PDO::PARAM_STR);
+        $insertar -> bindParam(':email', $data['email'], PDO::PARAM_STR);
+        $insertar -> bindParam(':total_compras', $data['total_compras'], PDO::PARAM_INT);
         $insertar -> execute();
         $result = $insertar -> rowCount();
         return $result;
@@ -22,13 +22,13 @@
         $this->conexion();
         $result = [];
         if (is_numeric($id)) {
-            $sql = 'update producto set nombre_producto=:nombre_producto=:nombre_producto, descripcion=:descripcion, precio=:precio, stock=:stock where id=:id;';
+            $sql = 'update usuario set nombre_completo=:nombre_completo, telefono=:telefono, contrasena=:contrasena, email=:email, total_compras=:total_compras where id=:id;';
             $modificar=$this->con->prepare($sql);
-            $modificar->bindParam(':nombre_producto',$data['nombre_producto'], PDO::PARAM_STR);
-            $modificar->bindParam(':descripcion',$data['descripcion'], PDO::PARAM_STR);
-            $precio = strval($data['precio']);
-            $modificar->bindParam(':precio',$data['precio'], PDO::PARAM_STR);
-            $modificar->bindParam(':stock',$data['stock'], PDO::PARAM_INT);
+            $modificar->bindParam(':nombre_completo',$data['nombre_completo'], PDO::PARAM_STR);
+            $modificar->bindParam(':telefono',$data['telefono'], PDO::PARAM_STR);
+            $modificar->bindParam(':contrasena',$data['contrasena'], PDO::PARAM_STR);
+            $modificar->bindParam(':email',$data['email'], PDO::PARAM_STR);
+            $modificar->bindParam(':total_compras',$data['total_compras'], PDO::PARAM_INT);
             $modificar->bindParam(':id',$id, PDO::PARAM_INT);
             $modificar->execute();
             $result= $modificar->rowCount();
@@ -40,7 +40,7 @@
         $this -> conexion();
         $result = [];
         if (is_numeric($id)) {
-            $sql = "delete from producto where id=:id;";
+            $sql = "delete from usuario where id=:id;";
             $eliminar = $this->con->prepare($sql);
             $eliminar -> bindParam(':id', $id, PDO::PARAM_INT);
             $eliminar -> execute();
@@ -52,7 +52,7 @@
     function readOne ($id){
         $this->conexion();
         $result = [];
-        $consulta = 'select * from producto where id=:id;';
+        $consulta = 'select * from usuario where id=:id;';
         $sql = $this->con->prepare($consulta);
         $sql->bindParam(":id",$id,PDO::PARAM_INT);
         $sql -> execute();
@@ -64,7 +64,7 @@
     function readAll (){
         $this -> conexion();
         $result = [];
-        $consulta ='select * from producto;';
+        $consulta ='select * from usuario;';
         $sql = $this->con->prepare ($consulta); 
         $sql -> execute();
         $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
