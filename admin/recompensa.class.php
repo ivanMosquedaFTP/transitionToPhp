91edec3149502dc80d0debc02975e9b27bea5dc4
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-08 11:40:57 -0600

    recompensa.class.php refactored

--- admin/recompensa.class.php
+++ admin/recompensa.class.php
@@ -1,18 +1,16 @@
 <?php
  require_once ('../sistema.class.php');
 
- class producto extends sistema {
+ class recompensa extends sistema {
     function create ($data){
         $result = [];
         $insertar = [];
         $this -> conexion();
-        $sql="insert into producto(nombre_producto, descripcion, precio, stock) values(:nombre_producto, :descripcion, :precio, :stock);";
+        $sql="insert into recompensa(usuario_id, descripcion, fecha_otorgada) values(:usuario_id, :descripcion, :fecha_otorgada);";
         $insertar = $this->con->prepare($sql);
-        $insertar -> bindParam(':nombre_producto', $data['nombre_producto'], PDO::PARAM_STR);
+        $insertar -> bindParam(':usuario_id', $data['usuario_id'], PDO::PARAM_STR);
         $insertar -> bindParam(':descripcion', $data['descripcion'], PDO::PARAM_STR);
-        $precio = strval($data['precio']);
-        $insertar -> bindParam(':precio', $data['precio'], PDO::PARAM_STR);
-        $insertar -> bindParam(':stock', $data['stock'], PDO::PARAM_INT);
+        $insertar -> bindParam(':fecha_otorgada', $data['fecha_otorgada'], PDO::PARAM_STR);
         $insertar -> execute();
         $result = $insertar -> rowCount();
         return $result;
@@ -22,13 +20,11 @@
         $this->conexion();
         $result = [];
         if (is_numeric($id)) {
-            $sql = 'update producto set nombre_producto=:nombre_producto=:nombre_producto, descripcion=:descripcion, precio=:precio, stock=:stock where id=:id;';
+            $sql = 'update recompensa set usuario_id=:usuario_id, descripcion=:descripcion, fecha_otorgada=:fecha_otorgada where id=:id;';
             $modificar=$this->con->prepare($sql);
-            $modificar->bindParam(':nombre_producto',$data['nombre_producto'], PDO::PARAM_STR);
+            $modificar->bindParam(':usuario_id',$data['usuario_id'], PDO::PARAM_STR);
             $modificar->bindParam(':descripcion',$data['descripcion'], PDO::PARAM_STR);
-            $precio = strval($data['precio']);
-            $modificar->bindParam(':precio',$data['precio'], PDO::PARAM_STR);
-            $modificar->bindParam(':stock',$data['stock'], PDO::PARAM_INT);
+            $modificar->bindParam(':fecha_otorgada',$data['fecha_otorgada'], PDO::PARAM_STR);
             $modificar->bindParam(':id',$id, PDO::PARAM_INT);
             $modificar->execute();
             $result= $modificar->rowCount();
@@ -40,7 +36,7 @@
         $this -> conexion();
         $result = [];
         if (is_numeric($id)) {
-            $sql = "delete from producto where id=:id;";
+            $sql = "delete from recompensa where id=:id;";
             $eliminar = $this->con->prepare($sql);
             $eliminar -> bindParam(':id', $id, PDO::PARAM_INT);
             $eliminar -> execute();
@@ -52,7 +48,7 @@
     function readOne ($id){
         $this->conexion();
         $result = [];
-        $consulta = 'select * from producto where id=:id;';
+        $consulta = 'select * from recompensa where id=:id;';
         $sql = $this->con->prepare($consulta);
         $sql->bindParam(":id",$id,PDO::PARAM_INT);
         $sql -> execute();
@@ -64,10 +60,10 @@
     function readAll (){
         $this -> conexion();
         $result = [];
-        $consulta ='select * from producto;';
+        $consulta ='select * from recompensa;';
         $sql = $this->con->prepare ($consulta); 
         $sql -> execute();
-        $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
+        $result = $sql -> fetchALL(PDO::FETCH_ASSOC);
         return $result;
     }
  }
