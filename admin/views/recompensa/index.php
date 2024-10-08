9d5cd6a24915ef7fd3bf461c8ddfe943ea4713b2
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-08 11:50:20 -0600

    admin/views/recompensa/index.php refactored

--- admin/views/recompensa/index.php
+++ admin/views/recompensa/index.php
@@ -1,31 +1,27 @@
 <?php require('views/header.php')?>
-  <h1>Productos</h1>
+  <h1>recompensas</h1>
   <?php if (isset($mensaje)): $app -> alerta($tipo, $mensaje); endif;?>
-  <a href="producto.php?accion=crear" class="btn btn-success">Nuevo</a>
+  <a href="recompensa.php?accion=crear" class="btn btn-success">Nueva</a>
   <table class="table">
   <thead>
     <tr>
       <th scope="col">Id</th>
-      <th scope="col">Nombre del producto</th>
+      <th scope="col">Id del usuario</th>
       <th scope="col">Descripcion</th>
-      <th scope="col">Precio</th>
-      <th scope="col">Stock</th>
-      <th scope="col">Fecha de publicación</th>
+      <th scope="col">Fecha otorgada</th>
     </tr>
   </thead>
   <tbody>
-    <?php foreach($productos as $producto): ?>
+    <?php foreach($recompensas as $recompensa): ?>
     <tr>
-      <td><?php echo $producto ['id']; ?></td>
-      <td><?php echo $producto ['nombre_producto']; ?></td>
-      <td><?php echo $producto ['descripcion']; ?></td>
-      <td><?php echo $producto ['precio']; ?></td>
-      <td><?php echo $producto ['stock']; ?></td>
-      <td><?php echo $producto ['fecha_publicacion']; ?></td>
+      <td><?php echo $recompensa ['id']; ?></td>
+      <td><?php echo $recompensa ['usuario_id']; ?></td>
+      <td><?php echo $recompensa ['descripcion']; ?></td>
+      <td><?php echo $recompensa ['fecha_otorgada']; ?></td>
       <td>
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-          <a href="producto.php?accion=actualizar&id=<?php echo $producto ['id']; ?>" class="btn btn-warning">Actualizar</a>
-          <a href="producto.php?accion=eliminar&id=<?php echo $producto ['id']; ?>" class="btn btn-danger">Eliminar</a>
+          <a href="recompensa.php?accion=actualizar&id=<?php echo $recompensa ['id']; ?>" class="btn btn-warning">Actualizar</a>
+          <a href="recompensa.php?accion=eliminar&id=<?php echo $recompensa ['id']; ?>" class="btn btn-danger">Eliminar</a>
         </div>
       </td>
     </tr>
