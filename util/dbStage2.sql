9c65275a4ced7ee23ae42ecdbd5b9a18a5a014bc
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-12 20:52:11 -0600

    working on the user system

---
+++ util/dbStage2.sql
@@ -0,0 +1,17 @@
+CREATE TABLE permiso(
+    id_permiso INT AUTO_INCREMENT PRIMARY KEY, 
+    permiso VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE rol(
+    id_rol INT AUTO_INCREMENT PRIMARY KEY, 
+    rol VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE rol_permiso(
+    id_rol INT NOT NULL, 
+    id_permiso INT NOT NULL, 
+    PRIMARY KEY (id_rol, id_permiso), 
+    FOREIGN KEY (id_rol) REFERENCES rol(id_rol), 
+    FOREIGN KEY (id_permiso) REFERENCES permiso(id_permiso)
+);
