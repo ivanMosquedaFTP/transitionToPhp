69348fddd2b7c821214a26d66c434f6f5b87793d
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-10-12 22:26:28 -0600

    added functions getRole() and getPrivilegios()

--- sistema.class.php
+++ sistema.class.php
@@ -9,5 +9,41 @@
         function alerta($tipo, $mensaje) {
             include('views/alert.php');
         }
+
+        function getRole($email) {
+            $this -> conexion();
+            $data = [];
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $sql = "select r.rol from usuario u inner join usuario_rol ur on u.id = ur.id_usuario
+                    inner JOIN rol r on r.id_rol = ur.id_rol
+                    where u.email = :email ";
+
+                $roles = $this->con->prepare($sql);
+                $roles->bindParam(":email",$email,PDO::PARAM_STR);
+                $roles->execute();
+                $data = $roles->fetchAll(PDO::FETCH_ASSOC);
+            }
+            
+            return $data;
+        }
+
+        function getPrivilegios($email) {
+            $this -> conexion();
+            $data = [];
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $sql = "select p.permiso from usuario u inner join usuario_rol ur on u.id = ur.id_usuario
+                        inner JOIN rol r on r.id_rol = ur.id_rol
+                        INNER JOIN rol_permiso rp on rp.id_rol = r.id_rol
+                        inner JOIN permiso p on p.id_permiso = rp.id_permiso
+                            where u.email = :email;";
+
+                $privilegio = $this->con->prepare($sql);
+                $privilegio->bindParam(":email",$email,PDO::PARAM_STR);
+                $privilegio->execute();
+                $data = $privilegio->fetchAll(PDO::FETCH_ASSOC);
+            }
+            
+            return $data;
+        }
     }
 ?>
