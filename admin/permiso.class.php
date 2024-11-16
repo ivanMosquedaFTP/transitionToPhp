3d37acdfc737b2b9516478d917272db19370a2cc
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-16 17:29:10 -0600

    formated permiso.class.php

--- admin/permiso.class.php
+++ admin/permiso.class.php
@@ -1,14 +1,14 @@
 <?php
  require_once ('../sistema.class.php');
 
- class rol extends sistema {
+ class permiso extends sistema {
     function create ($data){
         $result = [];
         $insertar = [];
         $this -> conexion();
-        $sql="insert into rol(rol) values(:rol);";
+        $sql="insert into permiso(permiso) values(:permiso);";
         $insertar = $this->con->prepare($sql);
-        $insertar -> bindParam(':rol', $data['rol'], PDO::PARAM_STR);
+        $insertar -> bindParam(':permiso', $data['permiso'], PDO::PARAM_STR);
         $insertar -> execute();
         $result = $insertar -> rowCount();
         return $result;
@@ -17,10 +17,10 @@
     function update ($id, $data){
         $this->conexion();
         $result = [];
-        $sql = 'update rol set rol=:rol where id_rol=:id_rol;';
+        $sql = 'update permiso set permiso=:permiso where id_permiso=:id_permiso;';
         $modificar=$this->con->prepare($sql);
-        $modificar->bindParam(':id_rol',$id, PDO::PARAM_INT);
-        $modificar->bindParam(':rol',$data['rol'], PDO::PARAM_STR);
+        $modificar->bindParam(':id_permiso',$id, PDO::PARAM_INT);
+        $modificar->bindParam(':permiso',$data['permiso'], PDO::PARAM_STR);
         $modificar->execute();
         $result= $modificar->rowCount();
         return $result;
@@ -29,9 +29,9 @@
     function delete ($id){
         $this -> conexion();
         $result = [];
-        $sql = "delete from rol where id_rol=:id_rol;";
+        $sql = "delete from permiso where id_permiso=:id_permiso;";
         $eliminar = $this->con->prepare($sql);
-        $eliminar -> bindParam(':id_rol', $id, PDO::PARAM_INT);
+        $eliminar -> bindParam(':id_permiso', $id, PDO::PARAM_INT);
         $eliminar -> execute();
         $result = $eliminar -> rowCount();
         return $result;
@@ -40,9 +40,9 @@
     function readOne ($id){
         $this->conexion();
         $result = [];
-        $consulta = 'SELECT * FROM rol where id_rol=:id_rol;';
+        $consulta = 'SELECT * FROM permiso where id_permiso=:id_permiso;';
         $sql = $this->con->prepare($consulta);
-        $sql->bindParam(":id_rol",$id,PDO::PARAM_INT);
+        $sql->bindParam(":id_permiso",$id,PDO::PARAM_INT);
         $sql -> execute();
 
         $result = $sql->fetch(PDO::FETCH_ASSOC);
@@ -52,7 +52,7 @@
     function readAll (){
         $this -> conexion();
         $result = [];
-        $consulta ='select * from rol';
+        $consulta ='select * from permiso';
         $sql = $this->con->prepare ($consulta); 
         $sql -> execute();
         $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
