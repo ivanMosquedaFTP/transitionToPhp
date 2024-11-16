03226a52c8b964206ee8a876bf0dc74939cafdc0
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-16 17:40:33 -0600

    fixed typos

--- admin/views/permiso/crear.php
+++ admin/views/permiso/crear.php
@@ -8,8 +8,8 @@
     <div class="col-md-10">
         <form method="post" action="permiso.php?accion=<?php if($accion=="crear"):echo('nuevo');else:echo('modificar&id='.$id);endif;?>">
         <div class="mb-3">
-            <label for="permiso" class="form-label">Permiso</label>
-            <input class="form-contpermiso" type="text" name="data[permiso]" placeholder="Escribe aqui el permiso" value="<?php if(isset($permisos["permiso"])):echo($permisos['permiso']);endif;?>" id="permiso"/>
+            <label for="permiso" class="form-label">Defina el permiso a continuación</label>
+            <input class="form-control" type="text" name="data[permiso]" placeholder="Escribe aqui el permiso" value="<?php if(isset($permisos["permiso"])):echo($permisos['permiso']);endif;?>" id="permiso"/>
         </div>
 
         <input type="submit" value="Guardar" name="data[enviar]" class="btn btn-primary w-100">
