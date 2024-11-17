617f4674edade4292a0761d5f068b4f9816791bc
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-17 00:00:27 -0600

    implemented delete method for producto crud

--- admin/producto.class.php
+++ admin/producto.class.php
@@ -6,74 +6,146 @@
         $result = [];
         $insertar = [];
         $this -> conexion();
-        $sql="insert into producto(nombre_producto, descripcion, precio, stock, foto) values(:nombre_producto, :descripcion, :precio, :stock, :foto);";
-        $insertar = $this->con->prepare($sql);
+        $this -> con -> beginTransaction();
 
-        $foto = $this -> uploadFoto();
+        try {
+            $sql="insert into producto(nombre_producto, descripcion, precio, stock, foto) values(:nombre_producto, :descripcion, :precio, :stock, :foto);";
+            $insertar = $this->con->prepare($sql);
 
-        $insertar -> bindParam(':nombre_producto', $data['nombre_producto'], PDO::PARAM_STR);
-        $insertar -> bindParam(':descripcion', $data['descripcion'], PDO::PARAM_STR);
-        $precio = strval($data['precio']);
-        $insertar -> bindParam(':precio', $data['precio'], PDO::PARAM_STR);
-        $insertar -> bindParam(':stock', $data['stock'], PDO::PARAM_INT);
-        $insertar -> bindParam(':foto',$foto, PDO::PARAM_STR);
-        $insertar -> execute();
-        $result = $insertar -> rowCount();
-        return $result;
+            $foto = $this -> uploadFoto();
+
+            $insertar -> bindParam(':nombre_producto', $data['nombre_producto'], PDO::PARAM_STR);
+            $insertar -> bindParam(':descripcion', $data['descripcion'], PDO::PARAM_STR);
+            $precio = strval($data['precio']);
+            $insertar -> bindParam(':precio', $data['precio'], PDO::PARAM_STR);
+            $insertar -> bindParam(':stock', $data['stock'], PDO::PARAM_INT);
+            $insertar -> bindParam(':foto',$foto, PDO::PARAM_STR);
+            $insertar -> execute();
+
+            $this -> con -> commit();
+
+            $result = $insertar -> rowCount();
+            return $result;
+        } catch (Exception $e) {
+            $this -> con -> rollBack();
+            echo''. $e -> getMessage() .'';
+        }
+
+        return false;
     }
 
     function update ($id, $data){
-        $this->conexion();
         $result = [];
-        if (is_numeric($id)) {
-            $sql = 'update producto set nombre_producto=:nombre_producto=:nombre_producto, descripcion=:descripcion, precio=:precio, stock=:stock, foto=:foto where id=:id;';
-            $modificar=$this->con->prepare($sql);
-            $modificar->bindParam(':nombre_producto',$data['nombre_producto'], PDO::PARAM_STR);
-            $modificar->bindParam(':descripcion',$data['descripcion'], PDO::PARAM_STR);
-            $precio = strval($data['precio']);
-            $modificar->bindParam(':precio',$data['precio'], PDO::PARAM_STR);
-            $modificar->bindParam(':stock',$data['stock'], PDO::PARAM_INT);
-            $modificar -> bindParam(':foto', $data['foto'], PDO::PARAM_STR);
-            $modificar->bindParam(':id',$id, PDO::PARAM_INT);
-            $modificar->execute();
-            $result= $modificar->rowCount();
+        $this->conexion();
+        $this -> con -> beginTransaction();
+
+        try {
+            if (!is_null($id) && is_numeric($id)) {
+                $sql = 'update producto set nombre_producto=:nombre_producto=:nombre_producto, descripcion=:descripcion, precio=:precio, stock=:stock, foto=:foto where id=:id;';
+                $modificar=$this->con->prepare($sql);
+                $modificar->bindParam(':nombre_producto',$data['nombre_producto'], PDO::PARAM_STR);
+                $modificar->bindParam(':descripcion',$data['descripcion'], PDO::PARAM_STR);
+                $precio = strval($data['precio']);
+                $modificar->bindParam(':precio',$data['precio'], PDO::PARAM_STR);
+                $modificar->bindParam(':stock',$data['stock'], PDO::PARAM_INT);
+                $modificar -> bindParam(':foto', $data['foto'], PDO::PARAM_STR);
+                $modificar->bindParam(':id',$id, PDO::PARAM_INT);
+                $modificar->execute();
+                $result= $modificar->rowCount();
+                $this -> con -> commit();
+                return $result;
+            }
+        } catch (Exception $e) {
+            $this -> con->rollback();
+            echo''. $e -> getMessage() .'';
         }
-        return $result;
+
+        return false;
     }
 
-    function delete ($id){
-        $this -> conexion();
-        $result = [];
-        if (!is_null($id) && is_numeric($id)) {
-            $sql = "delete from producto where id=:id;";
-            $eliminar = $this->con->prepare($sql);
-            $eliminar -> bindParam(':id', $id, PDO::PARAM_INT);
-            $eliminar -> execute();
-            $result = $eliminar -> rowCount();
+    function delete($id) {
+        $this->conexion();
+        $this->con->beginTransaction();
+    
+        try {
+            if (!is_null($id) && is_numeric($id)) {
+                $sql = "SELECT * FROM producto WHERE id = :id";
+                $stmt = $this->con->prepare($sql);
+                $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+                $stmt->execute();
+                $producto = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+                if ($producto) {
+                    $sql = "DELETE FROM producto WHERE id = :id";
+                    $stmt = $this->con->prepare($sql);
+                    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+                    $stmt->execute();
+    
+                    if ($stmt->rowCount() > 0) {
+                        if ($producto['foto'] && $producto['foto'] !== 'default.png') {
+                            $this->deleteImage($producto['foto']);
+                        }
+    
+                        $this->con->commit();
+                        return true;
+                    } else {
+                        throw new Exception("No se pudo eliminar el producto de la base de datos.");
+                    }
+                } else {
+                    throw new Exception("El producto no existe.");
+                }
+            } else {
+                throw new Exception("ID de producto no válido.");
+            }
+        } catch (Exception $e) {
+            $this->con->rollBack();
+            echo "Error: " . $e->getMessage();
+            return false;
         }
-        return $result;
     }
 
     function readOne ($id){
-        $this->conexion();
         $result = [];
-        $consulta = 'select * from producto where id=:id;';
-        $sql = $this->con->prepare($consulta);
-        $sql->bindParam(":id",$id,PDO::PARAM_INT);
-        $sql -> execute();
+        $this->conexion();
+        $this -> con->beginTransaction();
 
-        $result = $sql->fetch(PDO::FETCH_ASSOC);
-        return $result;
+        try {
+            $consulta = 'select * from producto where id=:id;';
+            $sql = $this->con->prepare($consulta);
+            $sql->bindParam(":id",$id,PDO::PARAM_INT);
+            $sql -> execute();
+
+            $result = $sql->fetch(PDO::FETCH_ASSOC);
+
+            $this -> con -> commit();
+
+            return $result;
+        } catch (Exception $e) {
+            $this -> con->rollback();
+            echo "". $e -> getMessage() ."";
+        }
+
+        return false;
     }
 
     function readAll(){
-        $this -> conexion();
         $result = [];
-        $consulta ='select * from producto;';
-        $sql = $this->con->prepare ($consulta); 
-        $sql -> execute();
-        $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
-        return $result;
+        $this -> conexion();
+        $this -> con -> beginTransaction();
+
+        try {
+            $consulta ='select * from producto;';
+            $sql = $this->con->prepare ($consulta); 
+            $sql -> execute();
+            $result = $sql -> fetchALL(PDO::FETCH_ASSOC);    
+            $this -> con -> commit();
+            return $result;
+        } catch (Exception $e) {
+            $this -> con->rollback();
+            echo ''. $e -> getMessage() .'';
+        }
+        
+        return false;
     }
 
     function uploadFoto() {
@@ -102,22 +174,17 @@
         return $default;
     }
 
-    function deleteImage($id) {
-        $this->conexion();
-
-        $sql = "SELECT foto FROM producto WHERE id = :id";
-        $stmt = $this->con->prepare($sql);
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-        $foto = $stmt->fetchColumn();
+    function deleteImage($filename) {
+        try {
+            $ruta_imagen = "C:\\xampp\\htdocs\\transitionToPhp\\image\\shop\\" . $filename;
     
-        if ($foto && $foto !== 'default.png') {
-            $ruta_imagen = "C:\\xampp\\htdocs\\transitionToPhp\\image\\shop\\" . $foto;
             if (file_exists($ruta_imagen)) {
-                print_r($ruta_imagen);
-                die();
-                // unlink($ruta_imagen);
+                if (!unlink($ruta_imagen)) {
+                    throw new Exception("No se pudo eliminar la imagen del sistema de archivos.");
+                }
             }
+        } catch (Exception $e) {
+            throw new Exception("Error en deleteImage: " . $e->getMessage());
         }
     }
  }
