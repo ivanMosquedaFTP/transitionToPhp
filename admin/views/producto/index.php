46d63469517327cbed41edaceb8597fc0de1c5d3
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-16 22:38:03 -0600

    deleted comments

--- admin/views/producto/index.php
+++ admin/views/producto/index.php
@@ -34,95 +34,11 @@
   </tbody>
 </table>
 
-<!-- <main>
-   <section class="articles">
-     <div class="row">
-       <div class="col-md-3">
-         <div class="card w-100" style="width: 18rem;">
-           <img src="../image/shop/1.webp" class=" card-img-top" alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Gorra new era</h5>
-             <p class="card-text">Gorra new era original por solo $400</p>
-             <a href="#" class="btn btn-primary">Ver listado</a>
-           </div>
-         </div>
-       </div>
-       <div class="col-md-3">
-         <div class="card w-100" style="width: 18rem;">
-           <img src="../image/shop/2.webp" class=" card-img-top" alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Gorra new era</h5>
-             <p class="card-text">Gorra new era original por solo $400</p>
-             <a href="#" class="btn btn-primary">Ver listado</a>
-           </div>
-         </div>
-       </div>
-       <div class="col-md-3">
-         <div class="card w-100" style="width: 18rem;">
-           <img src="../image/shop/3.webp" class=" card-img-top" alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Gorra new era</h5>
-             <p class="card-text">Gorra new era original por solo $400</p>
-             <a href="#" class="btn btn-primary">Ver listado</a>
-           </div>
-         </div>
-       </div>
-       <div class="col-md-3">
-         <div class="card w-100" style="width: 18rem;">
-           <img src="../image/shop/4.webp" class=" card-img-top" alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Gorra new era</h5>
-             <p class="card-text">Gorra new era original por solo $400</p>
-             <a href="#" class="btn btn-primary">Ver listado</a>
-           </div>
-         </div>
-       </div>
-     </div>
-     <br>
-     <div class="row">
-       <div class="col-md-3">
-         <div class="card w-100" style="width: 18rem;">
-           <img src="../image/shop/5.webp" class=" card-img-top" alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Gorra new era</h5>
-             <p class="card-text">Gorra new era original por solo $400</p>
-             <a href="#" class="btn btn-primary">Ver listado</a>
-           </div>
-         </div>
-       </div>
-       <div class="col-md-3">
-         <div class="card w-100" style="width: 18rem;">
-           <img src="../image/shop/6.webp" class=" card-img-top" alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Gorra new era</h5>
-             <p class="card-text">Gorra new era original por solo $400</p>
-             <a href="#" class="btn btn-primary">Ver listado</a>
-           </div>
-         </div>
-       </div>
-       <div class="col-md-3">
-         <div class="card w-100" style="width: 18rem;">
-           <img src="../image/shop/7.webp" class=" card-img-top" alt="...">
-           <div class="card-body">
-             <h5 class="card-title">Gorra new era</h5>
-             <p class="card-text">Gorra new era original por solo $400</p>
-             <a href="#" class="btn btn-primary">Ver listado</a>
-           </div>
-         </div>
-       </div>
-    </div>
-  </section>
-</main> -->
-
 <div class="row">
     <?php foreach ($productos as $producto): ?>
         <div class="col-md-4">
             <div class="card mb-4 shadow-sm">
-              <!-- <img src="<?php echo $producto['foto'] ?: '../image/shop/default.png'; ?>" class="card-img-top" alt="<?php echo htmlspecialchars($producto['nombre_producto']); ?>"> -->
               <img src="<?php 
-                          // echo '<pre/>';
-                          // print_r($producto);
-                          // die();
                           if(file_exists("../image/shop/".$producto['foto'])) {
                             echo("../image/shop/".$producto['foto']);
                           } else {
