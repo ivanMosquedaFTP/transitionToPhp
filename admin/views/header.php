1df4b987191d786a39fe6126626a5fec38b8cc4e
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-19 22:21:24 -0600

    filesystem changed

--- admin/views/header.php
+++ admin/views/header.php
@@ -25,7 +25,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="../index/about.html">Acerca</a>
+              <a class="nav-link" href="../index/about.php">Acerca</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="../index/contact.html">Contacto</a>
