82d125b9431d0ca5ff793a52a0d3b30d6344d155
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-21 04:20:17 -0600

    modularized the code snippet that registers users

--- index.php
+++ index.php
@@ -57,36 +57,7 @@
     </section>
 
     <div class="b-example-divider"></div>
-
-    <div class="container col-xl-10 col-xxl-8 px-4 py-5">
-      <div class="row align-items-center g-lg-5 py-5">
-        <div class="col-lg-7 text-center text-lg-start">
-          <h1 class="display-4 fw-bold lh-1 text-body-emphasis mb-3">Crea tu cuenta hoy mismo!</h1>
-          <p class="col-lg-10 fs-4">Al crear tu cuenta estarás automáticamente inscrito al programa de beneficios a
-            clientes frecuentes, dichos beneficios van desde descuentos hasta productos gratis!</p>
-        </div>
-        <div class="col-md-10 mx-auto col-lg-5">
-          <form class="p-4 p-md-5 border rounded-3 bg-body-tertiary">
-            <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="inName" placeholder="Nombre completo" required="true">
-              <label for="inName">Nombre completo</label>
-            </div>
-            <div class="form-floating mb-3">
-              <input type="email" class="form-control" id="inEmail" placeholder="nombre@ejemplo.com" required="true">
-              <label for="inEmail">Correo electrónico</label>
-            </div>
-            <div class="form-floating mb-3">
-              <input type="password" class="form-control" id="inPass" placeholder="Contrasena" required="true">
-              <label for="inPas">Contraseña</label>
-            </div>
-            <button class="w-100 btn btn-lg btn-primary" type="submit">Terminar registro</button>
-            <hr class="my-4">
-            <small class="text-body-secondary">Al presionar el botón declaras que estás de acuerdo con nuetros <a
-                href="index/termsAndConditions.html"> términos y condiciones</a></small>
-          </form>
-        </div>
-      </div>
-    </div>
+    <?php require('admin/views/registerUserSnippet.php');?>
     <br>
   </main>
 </center>
