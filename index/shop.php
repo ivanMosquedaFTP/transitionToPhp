2aafb35a0b74627670f4c3f247eb1173452e4c54
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-23 17:32:02 -0600

    added references

--- index/shop.php
+++ index/shop.php
@@ -1,92 +1,41 @@
 <?php require('../admin/views/header.php');?>
+<?php if (isset($mensaje)): $app -> alerta($tipo, $mensaje); endif;?>
   <main>
-    <section class="articles">
-      <div class="row">
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/1.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/2.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/3.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/4.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <br>
-      <div class="row">
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/5.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/6.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/7.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <div class="card w-100" style="width: 18rem;">
-            <img src="../image/shop/8.webp" class=" card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Gorra new era</h5>
-              <p class="card-text">Gorra new era original por solo $400</p>
-              <a href="#" class="btn btn-primary">Ver listado</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <div class="row">
+        <?php if (!empty($productos)): ?>
+            <?php foreach ($productos as $producto): ?>
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm">
+                    <div class="card-img-container">
+                        <img src="<?php 
+                                    if(file_exists("../image/shop/".$producto['foto'])) {
+                                    echo("../image/shop/".$producto['foto']);
+                                    } else {
+                                    echo("../image/shop/default.png");
+                                    }
+                                ?>" class="card-img-top">
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo htmlspecialchars($producto['nombre_producto']); ?></h5>
+                        <p class="card-text"><?php echo htmlspecialchars($producto['descripcion']); ?></p>
+                        <p class="card-text"><strong>Precio:</strong> $<?php echo number_format($producto['precio'], 2); ?></p>
+                        <p class="card-text"><strong>Stock:</strong> <?php echo htmlspecialchars($producto['stock']); ?></p>
+                        <p class="card-text"><small class="text-muted">Publicado: <?php echo htmlspecialchars($producto['fecha_publicacion']); ?></small></p>
+                        <p class="card-text"><small class="text-muted">Identificador: <?php echo htmlspecialchars($producto['id']); ?></small></p>
+                        <div class="d-flex justify-content-between align-items-center">
+                        <a href="producto.php?accion=actualizar&id=<?php echo $producto['id']; ?>" class="btn btn-primary">Editar</a>
+                        <a href="producto.php?accion=eliminar&id=<?php echo $producto['id']; ?>" class="btn btn-danger">Eliminar</a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+            <?php else: ?>
+            <tr>
+                <td colspan="7">No hay productos disponibles.</td>
+            </tr>
+        <?php endif; ?>
+    </div>
   </main>
   <br>
 <?php require('../admin/views/footer.php');?>
