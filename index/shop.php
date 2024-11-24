f2ef484a9643d1d3fb95b205cf3734638369d8cc
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-23 18:49:09 -0600

    added styles at shop.php

--- index/shop.php
+++ index/shop.php
@@ -17,11 +17,11 @@
                     </div>
                     <div class="card-body">
                         <h5 class="card-title"><?php echo htmlspecialchars($producto['nombre_producto']); ?></h5>
-                        <p class="card-text"><?php echo htmlspecialchars($producto['descripcion']); ?></p>
+                        <!-- <p class="card-text"><?php echo htmlspecialchars($producto['descripcion']); ?></p> -->
                         <p class="card-text"><strong>Precio:</strong> $<?php echo number_format($producto['precio'], 2); ?></p>
                         <p class="card-text"><strong>Stock:</strong> <?php echo htmlspecialchars($producto['stock']); ?></p>
                         <p class="card-text"><small class="text-muted">Publicado: <?php echo htmlspecialchars($producto['fecha_publicacion']); ?></small></p>
-                        <p class="card-text"><small class="text-muted">Identificador: <?php echo htmlspecialchars($producto['id']); ?></small></p>
+                        <!-- <p class="card-text"><small class="text-muted">Identificador: <?php echo htmlspecialchars($producto['id']); ?></small></p> -->
                         <div class="d-flex justify-content-between align-items-center">
                         <a href="producto.php?accion=toBeDefined&id=<?php echo $producto['id']; ?>" class="btn btn-success">Comprar</a>
                         </div>
