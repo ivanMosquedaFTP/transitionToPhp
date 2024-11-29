e6f0446d36109f502378f33bdd82f890ff5f7e2d
ivanMosquedaFTP <21031178@itcelaya.edu.mx>
2024-11-29 15:55:02 -0600

    refactored admin/views/venta/index.php

--- admin/views/venta/index.php
+++ admin/views/venta/index.php
@@ -9,6 +9,7 @@
       <th scope="col">Usuario</th>
       <th scope="col">Producto</th>
       <th scope="col">Cantidad</th>
+      <th scope="col">Monto</th>
       <th scope="col">Fecha de la venta</th>
       <th scope="col">Opciones</th>
     </tr>
@@ -20,6 +21,7 @@
       <td><?php echo $venta ['nombre_completo']; ?></td>
       <td><?php echo $venta ['nombre_producto']; ?></td>
       <td><?php echo $venta ['cantidad']; ?></td>
+      <td><?php echo $venta ['monto']; ?></td>
       <td><?php echo $venta ['fecha_venta']; ?></td>
       <td>
         <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
